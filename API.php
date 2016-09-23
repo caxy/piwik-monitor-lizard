7d847f7a0e02b33176a8c51aa2ada34d74b1c43d
Benjamin Doherty <bjd@bangpound.org>
2016-09-23 13:51:12 -0500

    make the report work.

--- API.php
+++ API.php
@@ -1,38 +1,57 @@
 <?php
 /**
- * Piwik - free/libre analytics platform
+ * Piwik - free/libre analytics platform.
  *
  * @link http://piwik.org
- * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
+
 namespace Piwik\Plugins\MonitorLizard;
 
 use Piwik\DataTable;
-use Piwik\DataTable\Row;
 
 /**
- * API for plugin MonitorLizard
+ * API for plugin MonitorLizard.
  *
  * @method static \Piwik\Plugins\MonitorLizard\API getInstance()
  */
 class API extends \Piwik\Plugin\API
 {
-
     /**
      * Another example method that returns a data table.
-     * @param int    $idSite
-     * @param string $period
-     * @param string $date
+     *
+     * @param int         $idSite
+     * @param string      $period
+     * @param string      $date
      * @param bool|string $segment
+     * @param bool        $expanded
+     * @param bool        $idSubtable
+     * @param bool        $flat
+     *
      * @return DataTable
      */
-    public function getEventsbyUser($idSite, $period, $date, $segment = false)
+    public function getActionsByUser($idSite, $period, $date, $segment = false, $expanded = false, $idSubtable = false, $flat = false)
     {
-        $table = new DataTable();
+        $instance = \Piwik\Plugins\Actions\API::getInstance();
 
-        $table->addRowFromArray(array(Row::COLUMNS => array('nb_visits' => 5)));
+        $dataTable = $instance->getPageTitles($idSite, $period, $date, $segment, $expanded, $idSubtable, true);
 
-        return $table;
+        return $dataTable;
+    }
+
+    /**
+     * @param $idSite
+     * @param $period
+     * @param $date
+     * @param bool $segment
+     *
+     * @return DataTable
+     */
+    public function getActionsForUser($idSite, $period, $date, $segment = false)
+    {
+        $instance = \Piwik\Plugins\UserId\API::getInstance();
+
+        return $instance->getUsers($idSite, $period, $date, $segment);
     }
 }
---
+++ Reports/GetActionsByUser.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform.
+ *
+ * @link http://piwik.org
+ *
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\MonitorLizard\Reports;
+
+use Piwik\Piwik;
+use Piwik\Plugin\Report;
+use Piwik\Plugin\ViewDataTable;
+use Piwik\Plugins\Actions\Columns\PageTitle;
+
+/**
+ * This class defines a new report.
+ *
+ * See {@link http://developer.piwik.org/api-reference/Piwik/Plugin/Report} for more information.
+ */
+class GetActionsByUser extends Report
+{
+    protected function init()
+    {
+        $this->category = 'General_Actions';
+        $this->name = Piwik::translate('MonitorLizard_ActionsByUser');
+        $this->dimension = new PageTitle();
+        $this->documentation = Piwik::translate('');
+
+        // This defines in which order your report appears in the mobile app, in the menu and in the list of widgets
+        $this->order = 12;
+
+        // Uncomment the next line if your report defines goal metrics
+        // $this->hasGoalMetrics = true;
+
+        // Uncomment the next line if your report should be able to load subtables. You can define any action here
+        $this->actionToLoadSubTables = 'getActionsForUser';
+
+        // Uncomment the next line if your report always returns a constant count of rows, for instance always
+        // 24 rows for 1-24hours
+        // $this->constantRowsCount = true;
+
+        // If a menu title is specified, the report will be displayed in the menu
+         $this->menuTitle = 'MonitorLizard_ActionsByUser';
+
+        // If a widget title is specified, the report will be displayed in the list of widgets and the report can be
+        // exported as a widget
+         $this->widgetTitle = 'MonitorLizard_ActionsByUser';
+    }
+
+    /**
+     * Here you can configure how your report should be displayed. For instance whether your report supports a search
+     * etc. You can also change the default request config. For instance change how many rows are displayed by default.
+     *
+     * @param ViewDataTable $view
+     */
+    public function configureView(ViewDataTable $view)
+    {
+        if (!empty($this->dimension)) {
+            $view->config->addTranslations(array('label' => $this->dimension->getName()));
+        }
+
+        // $view->config->show_search = false;
+        // $view->requestConfig->filter_sort_column = 'nb_visits';
+        // $view->requestConfig->filter_limit = 10';
+
+        $view->config->columns_to_display = array_merge(array('label'), $this->metrics);
+    }
+}
